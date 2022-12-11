9cac14daeff97a2ba14d79a0e58512f4e4887985
Yachna20 <thakreyachna20@gmail.com>
2022-12-11 22:02:37 +0530

    [Yachna]- Print fibonacci series

---
+++ LogicalProgramming/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace LogicalProgramming
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Logical Programming Problems");
+                Console.WriteLine("1.Fibonacci Series");
+                int check = Convert.ToInt32(Console.ReadLine());
+                switch (check)
+                {
+                    case 1:
+                        Console.WriteLine(" Create Fibonacci Series");
+                        FibonacciSeries series = new FibonacciSeries();
+                        series.Fibonacci();
+                        break;
+                    default:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
+
